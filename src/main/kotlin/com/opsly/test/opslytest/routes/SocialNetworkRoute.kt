562f41b2b83f51bb4d75791148bb46dc20581226
emerson <emerson@unovation.com.br>
2020-02-09 14:08:10 -0200

    the firt router test was created

--- src/main/kotlin/com/opsly/test/opslytest/routes/SocialNetworkRoute.kt
+++ src/main/kotlin/com/opsly/test/opslytest/routes/SocialNetworkRoute.kt
@@ -1,7 +1,18 @@
 package com.opsly.test.opslytest.routes
 
+import com.opsly.test.opslytest.handler.SocialNetworkHandler
+import com.opsly.test.opslytest.model.SocialEventsResult
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.BodyInserters
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.router
 
 @Configuration
-class SocialNetworkRoute {
+class SocialNetworkRoute(var socialNetworkHandler: SocialNetworkHandler) {
+
+    @Bean
+    fun route() = router {
+        GET("/") { ServerResponse.ok().body(socialNetworkHandler.findEvents(), SocialEventsResult::class.java) }
+    }
 }
--- src/test/kotlin/com/opsly/test/opslytest/routes/SocialNetworkRouteTest.kt
+++ src/test/kotlin/com/opsly/test/opslytest/routes/SocialNetworkRouteTest.kt
@@ -1,11 +1,35 @@
 package com.opsly.test.opslytest.routes
 
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import com.opsly.test.opslytest.handler.SocialNetworkHandler
+import com.opsly.test.opslytest.model.SocialEventsResult
 import io.kotlintest.specs.StringSpec
 import io.kotlintest.spring.SpringListener
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.reactive.server.WebTestClient
+import reactor.core.publisher.Mono
 
 @SpringBootTest
 class SocialNetworkRouteTest : StringSpec() {
 
     override fun listeners() = listOf(SpringListener)
+
+    init {
+        "it should return a valid json with its values"{
+            val expected = Mono.just(SocialEventsResult(listOf("tweet1"), listOf("statuses"), listOf("photos")))
+            val client = WebTestClient.bindToRouterFunction(SocialNetworkRoute(createMock(expected)).route()).build()
+            client.get()
+                    .uri("/")
+                    .exchange()
+                    .expectBody()
+                    .json("{\"twitter\":[\"tweet1\"],\"facebook\":[\"statuses\"],\"instagram\":[\"photos\"]}")
+        }
+    }
+
+    private fun createMock(events: Mono<SocialEventsResult> = Mono.just(SocialEventsResult())): SocialNetworkHandler {
+        return mock<SocialNetworkHandler> {
+            on { findEvents() } doReturn events
+        }
+    }
 }
