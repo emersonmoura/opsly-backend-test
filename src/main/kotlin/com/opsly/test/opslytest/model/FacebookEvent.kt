dbe58a333b28fc86707fbffa6ec796324236020a
emerson <emerson@unovation.com.br>
2020-02-09 11:22:16 -0200

    the model classes were created

---
+++ src/main/kotlin/com/opsly/test/opslytest/model/FacebookEvent.kt
@@ -0,0 +1,3 @@
+package com.opsly.test.opslytest.model
+
+data class FacebookEvent(val name: String?, val status: String?)
---
+++ src/main/kotlin/com/opsly/test/opslytest/model/TwitterEvent.kt
@@ -0,0 +1,3 @@
+package com.opsly.test.opslytest.model
+
+data class TwitterEvent(val username: String?, val tweet: String?)
