57b7ff071a8043fb43cfb7199469a7a8c7d19204
emerson <emerson@unovation.com.br>
2020-02-09 11:28:38 -0200

    the mockserver initial configuration was added to the socialNetwork test

--- src/test/kotlin/com/opsly/test/opslytest/handler/SocialNetworkHandlerTest.kt
+++ src/test/kotlin/com/opsly/test/opslytest/handler/SocialNetworkHandlerTest.kt
@@ -1,12 +1,33 @@
 package com.opsly.test.opslytest.handler
 
+import io.kotlintest.Spec
 import io.kotlintest.specs.StringSpec
 import io.kotlintest.spring.SpringListener
+import okhttp3.mockwebserver.MockWebServer
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class SocialNetworkHandlerTest : StringSpec() {
 
+    companion object {
+        const val MAIN_URL = "http://localhost:%s"
+    }
     override fun listeners() = listOf(SpringListener)
+
+    val mockServer = MockWebServer()
+
+    var baseUrl = ""
+
+    override fun beforeSpec(spec: Spec) {
+        super.beforeSpec(spec)
+        mockServer.start()
+        baseUrl = String.format(MAIN_URL, mockServer.port)
+    }
+
+    override fun afterSpec(spec: Spec) {
+        super.afterSpec(spec)
+        mockServer.shutdown()
+
+    }
 
 }
